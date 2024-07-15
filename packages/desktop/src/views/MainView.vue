0a398f30aa4a97c008f1a6a287426f5ad09a01f3
Acbox <850625057@qq.com>
2024-07-16 06:51:53 +0800

    feat: basic layout

--- packages/desktop/src/views/MainView.vue
+++ packages/desktop/src/views/MainView.vue
@@ -1,84 +1,23 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import Menubar from 'primevue/menubar'
-import InputText from 'primevue/inputtext'
-import Avatar from 'primevue/avatar'
-import Toolbar from 'primevue/toolbar'
-import IconField from 'primevue/iconfield'
-import InputIcon from 'primevue/inputicon'
-import Badge from 'primevue/badge'
-import Button from 'primevue/button'
-import SplitButton from 'primevue/splitbutton'
-import Textarea from 'primevue/textarea'
-import FloatLabel from 'primevue/floatlabel'
-
-const items2 = ref([
-  {
-    label: 'Update',
-    icon: 'pi pi-refresh',
-  },
-  {
-    label: 'Delete',
-    icon: 'pi pi-times',
-  },
-])
-
-const items = ref([
-  {
-    icon: 'pi pi-home',
-  },
-  {
-    icon: 'pi pi-star',
-  },
-  {
-    icon: 'pi pi-search'
-  },
-  {
-    icon: 'pi pi-envelope',
-  },
-])
+import MessageInput from './MessageInput.vue'
+import TopMenubar from './TopMenubar.vue'
+import FriendsList from './FriendsList.vue'
 </script>
 
 <template>
+  <header class="sticky mt-0">
+    <TopMenubar></TopMenubar>
+  </header>
   <div>
-    <Menubar :model="items" class="w-full">
-      <template #start>
-        <Avatar
-          image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png"
-          shape="circle"
-        />
-        <p class="ml-3 text-lg">Arona</p>
-      </template>
-      <template #item="{ item, props, hasSubmenu, root }">
-        <a v-ripple class="flex items-center" v-bind="props.action">
-          <span :class="item.icon" />
-        </a>
-      </template>
-      <template #end>
-        <InputText placeholder="Search" type="text" class="w-32 sm:w-auto" />
-      </template>
-    </Menubar>
-
-    <Toolbar>
-      <template #start>
-        <Button icon="pi pi-plus" class="mr-2" severity="secondary" text />
-        <Button icon="pi pi-images" class="mr-2" severity="secondary" text />
-        <Button icon="pi pi-upload" severity="secondary" text />
-      </template>
-
-      <template #center>
-        <Textarea
-          rows="1"
-          cols="35"
-          :auto-resize="false"
-          placeholder="Message"
-          :resizable="false"
-        />
-      </template>
-
-      <template #end>
-        <SplitButton label="Send" :model="items2"></SplitButton
-      ></template>
-    </Toolbar>
+    <div class="flex h-full float-left w-[25%] bg-gary-100">
+      <FriendsList class="float-left w-full h-full"/>
+    </div>
+    <div class="float-right min-w-[75%]">
+      <div class="bg-gary-100">
+        www
+      </div>
+      <MessageInput class="absolute bottom-0 w-[75%]"/>
+    </div>
   </div>
 </template>
---
+++ packages/desktop/src/views/MessageInput.vue
@@ -0,0 +1,40 @@
+<template>
+  <Toolbar>
+    <template #start>
+      <Button icon="pi pi-plus" class="mr-2" severity="secondary" text />
+      <Button icon="pi pi-images" class="mr-2" severity="secondary" text />
+      <Button icon="pi pi-upload" severity="secondary" text />
+    </template>
+
+    <template #center>
+      <Textarea rows="1" cols="35" :auto-resize="false" placeholder="Message" :resizable="false" />
+    </template>
+
+    <template #end>
+      <SplitButton label="Send" :model="items"></SplitButton>
+    </template>
+  </Toolbar>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import Toolbar from 'primevue/toolbar'
+import IconField from 'primevue/iconfield'
+import InputIcon from 'primevue/inputicon'
+import Badge from 'primevue/badge'
+import Button from 'primevue/button'
+import SplitButton from 'primevue/splitbutton'
+import Textarea from 'primevue/textarea'
+import FloatLabel from 'primevue/floatlabel'
+
+const items = ref([
+  {
+    label: 'Update',
+    icon: 'pi pi-refresh',
+  },
+  {
+    label: 'Delete',
+    icon: 'pi pi-times',
+  },
+])
+</script>
