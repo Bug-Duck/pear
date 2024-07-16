c349fe6f227aa097b9cc56aa7c26c35363bc3595
Hydrogen <29159838+hydrati@users.noreply.github.com>
2024-07-16 14:58:28 +0800

    fix: unused comp

--- packages/desktop/src/views/MainView.vue
+++ packages/desktop/src/views/MainView.vue
@@ -1,15 +1,11 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import MessageInput from './MessageInput.vue'
+import MessageInput from '../components/MessageInput.vue'
 
 </script>
 
 <template>
   <div class="flex-1 h-[calc(100%-30px)] relative ">
     <div class="w-auto h-[calc(100%-10px)] max-[] scrollbar overflow-y-scroll z-20 px-10 pb-[5rem] mt-[30px]">
-      <!-- <div>
-        a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>
-      </div> -->
 
 
     </div>
@@ -31,22 +27,5 @@ import MessageInput from './MessageInput.vue'
   transform: translateX(-50%);
 }
 
-.scrollbar {
-}
 
-.scrollbar::-webkit-scrollbar {
-  background: transparent;
-  width: 7px;
-}
-
-.scrollbar::-webkit-scrollbar-thumb {
-  background: #ffffffa0;
-  border-radius: 10px;
-  cursor: grab;
-}
-
-.scrollbar::-webkit-scrollbar-track {
-  background: transparent;
-  border-radius: 10px;
-}
 </style>
