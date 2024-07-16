af8e9ed9c31300136bb8a24edfda622fb809c156
Hydrogen <29159838+hydrati@users.noreply.github.com>
2024-07-16 17:18:21 +0800

    feat: message

---
+++ packages/desktop/src/components/MessageItem.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import Fieldset from 'primevue/fieldset'
+
+defineProps<{
+  name: string
+  date: Date
+}>()
+
+const emit = defineEmits<{
+  onDetails: []
+}>()
+</script>
+
+<template>
+  <Fieldset :legend="name" class="message select-none">
+    <slot></slot>
+    <p class="text-xs font-mono text-gray-600 mt-2 select-none">
+      {{ date.getUTCFullYear() }}-{{ date.getUTCMonth().toString().padStart(2, '0') }}-{{
+        date.getUTCDay().toString().padStart(2, '0')
+      }}
+      {{ date.getUTCHours().toString().padStart(2, '0') }}:{{ date.getUTCMinutes().toString().padStart(2, '0') }}:{{
+        date.getUTCSeconds().toString().padStart(2, '0')
+      }} (UTC+0000)
+      <a class="hover:underline hover:cursor-pointer" @click="emit('onDetails')"
+        >[Details]</a
+      >
+    </p>
+  </Fieldset>
+</template>
+
+<style scoped>
+.message,
+:deep(* > legend) {
+  background: transparent !important;
+}
+</style>
--- packages/desktop/src/views/MainView.vue
+++ packages/desktop/src/views/MainView.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-import MessageInput from '../components/MessageInput.vue'
-
+import MessageInput from '@/components/MessageInput.vue'
+import MessageItem from '@/components/MessageItem.vue';
 </script>
 
 <template>
-  <div class="flex-1 h-[calc(100%-30px)] relative ">
-    <div class="w-auto h-[calc(100%-10px)] max-[] scrollbar overflow-y-scroll z-20 px-10 pb-[5rem] mt-[30px]">
+  <div class="flex-1 h-[calc(100%-30px)] relative">
+    <div class="w-auto h-[calc(100%-10px)] scrollbar overflow-y-scroll z-20 px-10 pb-[5rem] mt-[30px]">
 
+      <MessageItem name="Acbox" :date="new Date()" />
 
     </div>
     <MessageInput class="min-w-[38rem] w-[50%] z-50 drawer absolute bottom-bar" />
@@ -27,5 +28,8 @@ import MessageInput from '../components/MessageInput.vue'
   transform: translateX(-50%);
 }
 
+.message, :deep(* >legend) {
+  background: transparent !important;
+}
 
 </style>
